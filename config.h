c81d7e5706f2d6063f1506106de3ad6f195bdd08
Neel <nk10399@gmail.com>
2020-11-21 21:33:42 +0530

    Change font to Font Awesome 5

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FontAwesome:size=13" };
+static const char *fonts[]          = { "Font Awesome 5 Free Solid:size=13" };
 static const char dmenufont[]       = "FontAwesome:size=13";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#222222";

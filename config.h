29f00096821c680e44472fbfc0462529578bcd40
Neel <nk10399@gmail.com>
2020-12-01 10:25:47 +0530

    Invoke dmenu with nosort option

--- config.h
+++ config.h
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-c", "-g", "5", "-l", "10", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-S", "-c", "-g", "5", "-l", "10", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *lockscreen[]  = { "lockscreen", NULL };
 static const char *killx[]  = { "killx", NULL };

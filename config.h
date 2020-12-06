6f026bafc71b77a209c386ff95bf219daf8eaff1
Neel <nk10399@gmail.com>
2020-12-06 19:00:37 +0530

    Change tags using scroll wheel
    mod+Scroll Down/Up(anywhere) - Next/Prev tag
    Scroll Down/Up(on tag bar) - Next/Prev tag

--- config.h
+++ config.h
@@ -134,5 +134,12 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+
+	{ ClkClientWin,         MODKEY,         Button4,        shiftview,    {.i = -1} },
+	{ ClkClientWin,         MODKEY,         Button5,        shiftview,    {.i = +1} },
+	{ ClkRootWin,           MODKEY,         Button4,        shiftview,    {.i = -1} },
+	{ ClkRootWin,           MODKEY,         Button5,        shiftview,    {.i = +1} },
+	{ ClkTagBar,            0,              Button4,        shiftview,    {.i = -1} },
+	{ ClkTagBar,            0,              Button5,        shiftview,    {.i = +1} },
 };
 

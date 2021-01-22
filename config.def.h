a0d12d61e78d3b7bd14aca007565433f5ecda6ac
Neel <nk10399@gmail.com>
2021-01-22 19:47:08 +0530

    Add scratchpad patch

--- config.def.h
+++ config.def.h
@@ -2,27 +2,28 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 7;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 20;        /* gaps between windows */
+static const unsigned int snap      = 20;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Font Awesome 5 Free Solid:size=13" };
-static const char dmenufont[]       = "agave Nerd Font:size=16";
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#222222";
-static const char col_gray3[]       = "#eeeeee";
-static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#ee6600";
+static const char *fonts[]          = { "Font Awesome 5 Free Solid:size=11:antialias=true:autohint=true",
+                                        "Recursive:style=Sans Casual:size=13:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Recursive:style=Sans Casual:size=13";
+static const char col_gray1[]       = "#282a36";
+static const char col_gray2[]       = "#000000";
+static const char col_gray3[]       = "#f8f8f2";
+static const char col_gray4[]       = "#f8f8f2";
+static const char col_cyan[]        = "#bd93f9";
 static const unsigned int baralpha = 0xff;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -39,14 +40,17 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Thunderbird",  NULL,       NULL,       0,       1,           -1 },
+	{ "Brave-browser",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Pulseeffects",  NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "flameshot",  NULL,       NULL,       0,       1,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.6; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -68,10 +72,12 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-c", "-g", "5", "-l", "10", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-bw", "0", "-g", "6", "-l", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *lockscreen[]  = { "lockscreen", NULL };
 static const char *killx[]  = { "killx", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, "-d", "120x34", NULL };
 
 #include "shiftview.c"
 
@@ -79,6 +85,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY|ControlMask,           XK_l,      spawn,          {.v = lockscreen } },
 	{ MODKEY|ShiftMask,             XK_q,      spawn,          {.v = killx } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -134,5 +141,12 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+
+	{ ClkClientWin,         MODKEY,         Button4,        shiftview,    {.i = -1} },
+	{ ClkClientWin,         MODKEY,         Button5,        shiftview,    {.i = +1} },
+	{ ClkRootWin,           MODKEY,         Button4,        shiftview,    {.i = -1} },
+	{ ClkRootWin,           MODKEY,         Button5,        shiftview,    {.i = +1} },
+	{ ClkTagBar,            0,              Button4,        shiftview,    {.i = -1} },
+	{ ClkTagBar,            0,              Button5,        shiftview,    {.i = +1} },
 };
 
